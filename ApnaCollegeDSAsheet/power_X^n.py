eed89654d5e289068f98e3f8e80989f2b03e8965
Hamim Mahmud <hamim.mahmud207@gmail.com>
2026-09-08 01:15:39 +0600

    Adding DSA Sheet question 6

--- ApnaCollegeDSAsheet/power_X^n.py
+++ ApnaCollegeDSAsheet/power_X^n.py
@@ -1,1 +1,19 @@
 # Question Link: https://leetcode.com/problems/powx-n/description/
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        ans=1
+        m=n
+        n=abs(n)
+        while(n>0):
+            if(n%2==1):
+                n=n-1
+                ans=ans*x
+            else:
+                n=n/2
+                x=x*x
+        
+        if(m<0):
+            return 1.0/ans
+        else:
+            return ans
