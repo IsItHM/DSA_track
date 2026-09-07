1215470148d0686dbe04a4c493d34f65f825b370
Hamim Mahmud <hamim.mahmud207@gmail.com>
2026-09-07 10:51:29 +0600

    Adding 2nd solve of DSASheet

--- ApnaCollegeDSAsheet/Find_MissingandRepeatingValues.py
+++ ApnaCollegeDSAsheet/Find_MissingandRepeatingValues.py
@@ -1,3 +1,22 @@
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        ans = []
+        seen = set()
+        n = len(grid)
+        a = -1                      # the repeated value
 
+        actual_sum = 0
+        for i in range(n):
+            for j in range(n):
+                val = grid[i][j]
+                actual_sum += val
+                if val in seen:
+                    a = val
+                    ans.append(a)
+                seen.add(val)
 
-
+        m = n * n
+        exp_sum = m * (m + 1) // 2
+        b = exp_sum + a - actual_sum
+        ans.append(b)
+        return ans
