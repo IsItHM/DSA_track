f8db6613e84fdb3a0558f20538d75d6a7f0ea6be
Hamim Mahmud <hamim.mahmud207@gmail.com>
2026-09-07 17:24:21 +0600

    DSA Sheet problem 4

---
+++ ApnaCollegeDSAsheet/Single Number.py
@@ -0,0 +1,7 @@
+#Question Link: https://leetcode.com/problems/single-number/description/
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = 0
+        for val in nums:
+            ans ^= val
+        return ans
