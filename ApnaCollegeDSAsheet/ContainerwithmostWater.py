d381041caa75f544be468d03f051ce071d2dd022
Hamim Mahmud <hamim.mahmud207@gmail.com>
2026-09-09 02:34:00 +0600

    Adding container with most water solve

--- ApnaCollegeDSAsheet/ContainerwithmostWater.py
+++ ApnaCollegeDSAsheet/ContainerwithmostWater.py
@@ -1,1 +1,14 @@
 # https://leetcode.com/problems/container-with-most-water/description/
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        lp=0
+        rp=len(height)-1
+        maximum=0
+        for h in height:
+            current= (rp-lp)*min(height[lp],height[rp])
+            maximum=max(maximum,current)
+            if lp<rp :
+                lp=lp+1
+            else:
+                rp=rp-1
+        return maximum
