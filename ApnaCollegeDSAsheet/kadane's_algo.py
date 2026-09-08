2b7569a788ecf5fe3976e731239c65a07c977b23
Hamim Mahmud <hamim.mahmud207@gmail.com>
2026-09-08 21:21:29 +0600

    Adding kadane's algo solve

--- ApnaCollegeDSAsheet/kadane's_algo.py
+++ ApnaCollegeDSAsheet/kadane's_algo.py
@@ -1,1 +1,13 @@
 # Question link: https://leetcode.com/problems/maximum-subarray/description/
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        current=0
+        maxi= float('-inf')
+        for num in nums:
+            current=current+num
+            if(current>maxi):
+                maxi=current
+            if(current<0):
+                current=0
+        return maxi
